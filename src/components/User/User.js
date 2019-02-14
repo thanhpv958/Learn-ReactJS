d85441915ebe1dc706916fe9792b15dce629e6a1
thanhpv958 <thanhpv958@gmail.com>
2019-02-14 11:21:38 +0700

    add new user

--- src/components/User/User.js
+++ src/components/User/User.js
@@ -1,17 +1,42 @@
 import React, { Component } from 'react';
 import ListUser from './ListUser';
 import AddUser from './AddUser';
+import dataUser from '../DataUser.json';
 
 class User extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            dataUser: dataUser
+        }
+    }
+    
+    getNewUser = (name, phone, role) => {
+        let user = {};
+        user.id = "";
+        user.name = name;
+        user.phone = phone;
+        user.role = role;
+
+        let users = this.state.dataUser;
+        users.push(user);
+        this.setState({
+            dataUser: users
+        });
+    };
+
     render() {
         return (
             <div className="row">
                 <ListUser
                     showFormAddUser={ this.props.showFormAddUser }
-                    dataUser={ this.props.dataUser }
+                    dataUser={ this.state.dataUser }
                 />
                 
-                <AddUser showFormAddUser={ this.props.showFormAddUser }/>
+                <AddUser
+                    showFormAddUser={ this.props.showFormAddUser }
+                    getNewUser={ (name, phone, role) => this.getNewUser(name, phone, role) } />
             </div>
         );
     }

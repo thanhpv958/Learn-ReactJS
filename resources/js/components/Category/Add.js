7af11d8ae0e1526e30db6756838b7700c53d6620
thanhpv958 <thanhpv958@gmail.com>
2019-07-25 15:58:56 +0700

    action add new category

--- resources/js/components/Category/Add.js
+++ resources/js/components/Category/Add.js
@@ -1,12 +1,59 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 export default class Add extends Component {
+
+    constructor()
+    {
+        super();
+        this.state = {
+        };
+    }
+
+    /**
+     * Request server to post data
+     * @param {object} data 
+     */
+    storeCategory(data)
+    {
+        return axios.post('http://127.0.0.1:8001/api/categories', data);
+    }
+
+    /**
+     * Get data store
+     * @param {*} event 
+     */
+    getDataStore(event)
+    {
+        let name = event.target.name;
+        let value = event.target.value;
+
+        this.setState({
+            [name]: value
+        });
+    }
+
+    /**
+     * Submit data
+     * @param {object} event 
+     */
+    onSubmit(event)
+    {
+        event.preventDefault();
+       
+        let category = {
+            name: this.state.name
+        };
+
+        return this.storeCategory(category);
+    }
+
     render() {
         return (
             <div>
-                <form>
+                <form onSubmit={ () => this.onSubmit(event) }>
                     <div className="form-group">
-                        <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Category name" />
+                        <input type="text" name="name" onChange={ () => this.getDataStore(event) } className="form-control" placeholder="Category name" />
                     </div>
 
                     <button type="submit" className="btn btn-primary">Submit</button>
